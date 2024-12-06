0b5347cf6e2ca2e64f1d521fee8140492458920d
Guilherme Henrique Soares França <ghsfranca@gmail.com>
2024-12-05 21:12:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./db/db.json')
+
+const middlewares = jsonServer.defaults({ noCors: true })
+server.use(middlewares)
+server.use(router)
+
+server.listen(3000, () => {
+    console.log(`JSON Server is running em http://localhost:3000`)
+})
